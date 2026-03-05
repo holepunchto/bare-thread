41985104a82f2a280237974b3387c49236553c87
Yasser Nascimento <dev@yasser.email>
2026-03-05 11:28:11 -0300

    Fix test

--- test.js
+++ test.js
@@ -19,9 +19,10 @@ test('Thread.cpu', (t) => {
 
 test('name property', (t) => {
   const thread = new Thread(require.resolve('./test/fixtures/basic/index.js'))
-  thread.join()
 
   thread.name = 'bare-test'
 
   t.is(thread.name, 'bare-test')
+
+  thread.join()
 })
